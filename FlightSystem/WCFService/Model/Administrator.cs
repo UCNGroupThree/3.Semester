1cdf4d6a4acbdcb97b8022bf039a4c9ef9cd1e75
is12b <zoinoz@gmail.com>
2015-03-04 12:17:22 +0100

    EntityFramework
    Model / Entityframework

---
+++ FlightSystem/WCFService/Model/Administrator.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace WCFService.Model
+{
+    [DataContract(IsReference = true)]
+    public class Administrator
+    {
+        [DataMember]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [DataMember]
+        public string Username { get; set; }
+
+        [DataMember]
+        public string Password { get; set; }
+
+        [DataMember]
+        [Timestamp]
+        public byte[] Concurrency { get; set; }
+    }
+}
