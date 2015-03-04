1c7f7c5323b16e7a97e63ff005507e0633386ad5
CodingDK <nonames.dk@gmail.com>
2015-03-04 17:04:15 +0100

    Added Airport and Route Models

---
+++ FlightSystem/WCFService/Model/Airport.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFService.Model {
+
+    [DataContract(IsReference = true)]
+    [KnownType(typeof(Route))]
+    [KnownType(typeof(TimeZone))]
+    public class Airport {
+
+        [DataMember]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] //Fortæller at ID skal være IDENTITY
+        public int ID { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string ShortName { get; set; }
+
+        [DataMember]
+        public string City { get; set; }
+
+        [DataMember]
+        public string Country { get; set; }
+
+        [DataMember]
+        public decimal Latitude { get; set; }
+
+        [DataMember]
+        public decimal Longtitude { get; set; }
+
+        [DataMember]
+        public decimal Altitude { get; set; }
+
+        [DataMember]
+        public TimeZone TimeZone { get; set; }
+
+        [DataMember]
+        public List<Route> Routes { get; set; }
+
+        [DataMember]
+        [Timestamp]
+        public byte[] Concrrency { get; set; }
+        
+    }
+}
---
+++ FlightSystem/WCFService/Model/Route.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace WCFService.Model {
+    
+    [DataContract(IsReference = true)]
+    [KnownType(typeof(Airport))]
+    [KnownType(typeof(Flight))]
+    public class Route {
+
+        [DataMember]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [DataMember]
+        public Airport LocationOne { get; set; }
+
+        [DataMember]
+        public Airport LocationTwo { get; set; }
+
+        [DataMember]
+        public List<Flight> Flights { get; set; }
+
+        [DataMember]
+        [Timestamp]
+        public byte[] Concurrency { get; set; }
+
+    }
+}
