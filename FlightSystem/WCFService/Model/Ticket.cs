3754e3c85489c3c3088670460624047c8c2b6e6c
is12b <zoinoz@gmail.com>
2015-03-05 10:03:27 +0100

    Fixing something!
    Fixing something!

--- FlightSystem/WCFService/Model/Ticket.cs
+++ FlightSystem/WCFService/Model/Ticket.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
 using System.Runtime.Serialization;
-using System.Text;
 
 namespace WCFService.Model {
 
