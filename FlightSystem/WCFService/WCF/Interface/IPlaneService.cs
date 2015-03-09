dd02f22b1a96f4c9cf1be592738b49e39db5bc71
CodingDK <nonames.dk@gmail.com>
2015-03-09 12:37:31 +0100

    Added IPlaneService and IRouteService

--- FlightSystem/WCFService/WCF/Interface/IPlaneService.cs
+++ FlightSystem/WCFService/WCF/Interface/IPlaneService.cs
@@ -1,8 +1,19 @@
 ﻿using System.ServiceModel;
+using WCFService.Model;
 
 namespace WCFService.WCF.Interface {
     [ServiceContract]
     public interface IPlaneService {
-         
+        [OperationContract]
+        int AddPlane(Plane plane);
+
+        [OperationContract]
+        void UpdatePlane(Plane plane);
+
+        [OperationContract]
+        void DeletePlane(Plane plane);
+
+        [OperationContract]
+        Plane GetPlane(int id);
     }
 }
