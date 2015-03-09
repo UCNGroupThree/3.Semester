9a2ae5ccfdb60fcccbb83e0f7632a964a3fee008
CodingDK <nonames.dk@gmail.com>
2015-03-09 12:19:50 +0100

    State enum

--- FlightSystem/WCFService/Model/SeatReservation.cs
+++ FlightSystem/WCFService/Model/SeatReservation.cs
@@ -3,11 +3,11 @@ using System.Runtime.Serialization;
 
 namespace WCFService.Model {
     [DataContract(IsReference = true)]
-    //[KnownType(typeof (SeatState))]
+    [KnownType(typeof (SeatState))]
     [KnownType(typeof (Seat))]
     [KnownType(typeof (Flight))]
     public class SeatReservation {
-        public SeatReservation(string state, string name, Seat seat, Flight flight) {
+        public SeatReservation(SeatState state, string name, Seat seat, Flight flight) {
             this.State = state;
             this.Name = name;
             this.Seat = seat;
@@ -31,6 +31,6 @@ namespace WCFService.Model {
         public string Name { get; set; }
 
         [DataMember]
-        public string State { get; set; }
+        public SeatState State { get; set; }
     }
 }
