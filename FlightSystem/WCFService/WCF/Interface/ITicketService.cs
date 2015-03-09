1be86cf087d10f667ceb5e958ea28c8b4757719d
Jakob Korsgaard <mma-jk@hotmail.com>
2015-03-09 12:42:39 +0100

    ITicket

--- FlightSystem/WCFService/WCF/Interface/ITicketService.cs
+++ FlightSystem/WCFService/WCF/Interface/ITicketService.cs
@@ -1,8 +1,22 @@
-﻿using System.ServiceModel;
+﻿using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel;
+using WCFService.Model;
 
 namespace WCFService.WCF.Interface {
     [ServiceContract]
     public interface ITicketService {
-         
+
+        [OperationContract]
+        void AddTicket(Ticket ticket);
+
+        [OperationContract]
+        void UpdateTicket(Ticket ticket);
+
+        [OperationContract]
+        void DeleteTicket(Ticket ticket);
+
+        [OperationContract]
+        Ticket GetTicket(int id);
+
     }
 }
