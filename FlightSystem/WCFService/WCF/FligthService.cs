a3215df00203a752a3a21ebb0dcac7d5b48bd516
Jakob Korsgaard <mma-jk@hotmail.com>
2015-03-15 01:49:28 +0100

    FlightService added

---
+++ FlightSystem/WCFService/WCF/FligthService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using WCFService.WCF.Interface;
+using WCFService.Model;
+using System.ServiceModel;
+
+namespace WCFService.WCF {
+    public class FligthService : IFlightService {
+        private readonly FlightDB db = new FlightDB();
+
+        public int AddFlight(Flight flight)
+        {
+            if (flight == null) {
+                throw new FaultException("Nullpointer Exception"); //TODO vores egen Nullpointer Exception?
+            }
+            try {
+                db.Flights.Add(flight);
+                db.SaveChanges();
+            }
+            catch (Exception ex) {
+
+                Console.WriteLine(ex.Message); //TODO DEBUG MODE?
+                throw new FaultException("The database was unable to insert the record");
+            }
+           
+        }
+
+        public void UpdateFlight(Flight flight)
+        {
+            if (flight == null)
+            {
+                throw new FaultException("Nullpointer Exception"); //TODO vores egen Nullpointer Exception?
+            }
+            try {
+                db.Flights.Attach(flight);
+                db.Entry(flight).State = EntityState.Modified;
+                db.SaveChanges();
+
+            }
+            catch (Exception ex) {
+
+                Console.WriteLine(ex.Message); //TODO DEBUG MODE?
+                throw new FaultException("The database was unable to update the record");
+            }
+        }
+
+        public void DeleteFlight(Flight flight)
+        {
+            if (flight == null) {
+                throw new FaultException("Nullpointer Exception"); //TODO vores egen Nullpointer Exception?
+            }
+            try {
+                db.Flights.Remove(flight);
+                db.SaveChanges();
+            }
+            catch (Exception) {
+
+                Console.WriteLine(ex.Message); //TODO DEBUG MODE?
+                throw new FaultException("The database was unable to update the record");
+            }
+        }
+
+        public Flight GetFlight(int id) {
+            return db.Flights.SingleOrDefault(x => x.ID == id);
+        }
+    }
+}
