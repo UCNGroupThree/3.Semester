0029f3fa91dacfd9c293d10c298e2609a656aef8
Nick D Pedersen <zoinoz@gmail.com>
2015-03-20 21:16:32 +0100

    CreateFlight added to FlightCtr

--- FlightSystem/FlightAdmin/Controller/FlightCtr.cs
+++ FlightSystem/FlightAdmin/Controller/FlightCtr.cs
@@ -1,10 +1,46 @@
-﻿namespace FlightAdmin.Controller {
+﻿using System;
+using FlightAdmin.FlightService;
+
+namespace FlightAdmin.Controller {
     public class FlightCtr {
 
+        public Flight CreateFlight(DateTime arrival, DateTime departure, Plane plane, decimal price) {
+            Flight flight = null;
 
+            if (FlightValidation(arrival, departure, plane, price)) {
+                flight = new Flight {ArrivalTime = arrival, DepartureTime = departure, Plane = plane, Price = price};
 
-        public void CreateFlight() {
+                try {
+                    using (var client = new FlightServiceClient()) {
+                        flight.ID = client.AddFlight(flight);
+                    }
+                } catch (Exception e) {
+                    throw new Exception("WCF Service Exception"); //TODO Better Exception
+                }
+
+            } else {
+                throw new Exception("FlightValidation Exception"); //TODO Better Exception
+            }
+
+            return flight;
+        }
+
+        public Flight UpdateFlight(Flight flight, DateTime arrival, DateTime departure, Plane plane, decimal price) {
+           
+            using (var client = new FlightServiceClient()) {
             
+            }
+
+            return flight;
+
+        }
+
+
+
+        private bool FlightValidation(DateTime arrival, DateTime departure, Plane plane, decimal price) {
+            bool ret = (plane != null);
+
+            return ret;
         }
     }
 }
