c7570856afeef7d9a0387b8f1a3b8ae130342ef7
Jakob Korsgaard <mma-jk@hotmail.com>
2015-03-24 10:38:17 +0100

    Postal interface addad

---
+++ FlightSystem/WCFService/WCF/Interface/IPostalService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using WCFService.Model;
+
+namespace WCFService.WCF.Interface
+{
+    [ServiceContract]
+    interface IPostalService {
+
+        [OperationContract]
+        int AddPostal(Postal postal);
+
+        [OperationContract]
+        Postal UpdatePostal(Postal postal);
+
+        [OperationContract]
+        int DeletePostal(Postal postal);
+
+        [OperationContract]
+        Postal GetPostal(Postal postal);
+
+
+    }
+}
