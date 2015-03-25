e4f51a3d7b79fdfcebcd82969c56ae3e56167056
CodingDK <nonames.dk@gmail.com>
2015-03-25 08:41:44 +0100

    AirportCtr added GetAirportsByCountry

--- FlightSystem/FlightAdmin/Controller/AirportCtr.cs
+++ FlightSystem/FlightAdmin/Controller/AirportCtr.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using FlightAdmin.GUI;
 using FlightAdmin.MainService;
 
@@ -19,13 +20,35 @@ namespace FlightAdmin.Controller {
                     Altitude = altitude,
                     TimeZone = timeZone //TODO ASD ENTITY SEARCH
                 };
-            } catch (Exception) {
+                using (AirportServiceClient client = new AirportServiceClient()) {
+                    airport.ID = client.AddAirport(airport);
+                }
+            } catch (Exception ex) {
+                airport = null;
+                Console.WriteLine(@"CreateAirport Exception: " + ex);
                 //TODO Exception Handler
                 throw;
             }
 
             return airport;
         }
+
+
+        public List<Airport> GetAirportsByCountry(string country) {
+            List<Airport> list;
+            try {
+                using (AirportServiceClient client = new AirportServiceClient()) {
+                    list = client.GetAirportsByCountry(country);
+                }
+            } catch (Exception ex) {
+                list = null;
+                Console.WriteLine(@"GetAirportsByCountry Exception: " + ex);
+                //TODO Exception Handler
+                //throw;
+            }
+            return list;
+        }
+
         
 
     }
