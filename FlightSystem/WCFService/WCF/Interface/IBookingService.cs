ec691b43023269a06bcee5e4a1a50ed1adc7c5e1
CodingDK <nonames.dk@gmail.com>
2015-04-29 22:00:03 +0200

    Some testing.. Just ignore it

---
+++ FlightSystem/WCFService/WCF/Interface/IBookingService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using WCFService.Model;
+
+namespace WCFService.WCF.Interface {
+    [ServiceContract]
+    public interface IBookingService {
+
+        [OperationContract]
+        int AddOne();
+
+        [OperationContract]
+        List<Flight> GetFlights(int fromId, int toId, int seats, DateTime dateTime);
+
+        [OperationContract]
+        List<SeatReservation> MakeSeatsOccupiedRandom(List<Flight> flights, int seats);
+
+        [OperationContract]
+        void Complete();
+
+    }
+}
